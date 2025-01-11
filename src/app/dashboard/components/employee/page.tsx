2ff310f038fff48bea77577e14361823f71d7238
itaminul <itaminul@gmail.com>
2025-01-11 22:59:24 +0600

    breadcrumbs modify

--- src/app/dashboard/components/employee/page.tsx
+++ src/app/dashboard/components/employee/page.tsx
@@ -2,8 +2,13 @@ import Breadcrumbs, { breadcrumbItem } from "../breadcrumbs/breadcrumbs";
 
 export default async function Page() {
   const breadcrumbItems: breadcrumbItem[] = [
-    {href: "/", label: "Home", icon: 'home'}
-  ]
+    {href: "/", label: "Home", icon: 'home'},
+     { href: "/menu", label: "Menu" },
+    { href: "/menu/submenu", label: "Submenu" },
+    { href: "/menu/submenu/current-page", label: "Current Page" },
+  ];
+  
+  
   return <div>
     <Breadcrumbs items={breadcrumbItems} />
   </div>;
