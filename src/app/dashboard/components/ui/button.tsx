9d865a96d9af12d9abc9e7d2997777774a704206
itaminul <itaminul@gmail.com>
2025-01-14 23:03:59 +0600

    dahsbord desing

---
+++ src/app/dashboard/components/ui/button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "default" | "outline" | "ghost";
+  size?: "default" | "sm" | "lg";
+}
+
+const Button = React.forwardRef < HTML;
